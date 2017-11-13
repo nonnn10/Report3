1299da8345477ba1af91f607d7ecbb3e2412478d
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-11-13 20:00:59 +0900

    ２回目 commit

--- src/main/jp/ac/uryukyu/ie/e175755/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175755/Enemy.java
@@ -47,8 +47,12 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
+        if(isDead() != true){
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+
+
+        }
     }
 
     /**
